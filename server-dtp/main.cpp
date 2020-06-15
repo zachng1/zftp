4ab41dbf68c345bc6f6299c8d7960f1d8b6f37dd
Zac Hing <zachng1@gmail.com>
2020-06-15 22:27:45 +1200

    Dummy file for DTP

--- server-dtp/main.cpp
+++ server-dtp/main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
-#ifdef __linux__
+
+//C Socket headers:
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h> 
-#endif
-#ifdef _WIN32
-#include <WinSock2.h>
-#endif
+#include <unistd.h>
 
 
-int main() {
+
+int main(int argc, char ** argv) {
+    int writeToPi = std::stoi(std::string(argv[1]));
+    int readFromPi = std::stoi(std::string(argv[2]));
     
+    //signal to PI that we are ready to start sending and recieving data
+    char readyBuf[1]{'A'};
+    if (write(writeToPi, readyBuf, 1) < 0) {
+        std::cout << "Error writing to PI" << std::endl;
+        return 1;
+    };
+    while (1);
+    return 0;
 }

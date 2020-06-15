08e4c5e2f1d2109f79a888076739a6af61539579
Zac Hing <zachng1@gmail.com>
2020-06-15 22:27:32 +1200

    Centralise launching of DTP and PI

--- Makefile
+++ Makefile
@@ -1,22 +1,26 @@
 CC = g++
-compile-flags = -Wall -Wextra -pedantic -std=c++17
+compile-flags = -g -Wall -Wextra -pedantic -std=c++17
 link-flags = -pthread
 
 objects-dtp = $(addprefix server-dtp/,main.o)
-objects-pi = $(addprefix server-pi/,main.o listener.o user.o)
-exe = main
+objects-pi = $(addprefix server-pi/,listener.o user.o handlerfunctions.o main.o)
+objects-launcher = launcher.cpp
 exe-dtp = server-dtp/dtp
 exe-pi = server-pi/pi
+exe-launcher = launcher
 
-default:
-	@echo Please specify either make dtp or make pi 
+$(exe-launcher): $(exe-pi) $(exe-dtp)
+	$(CC) $(compile-flags) -o $(exe-launcher) $(objects-launcher) $(link-flags)
 
+$(exe-pi): $(objects-pi)
+	$(CC) $(compile-flags) -o $(exe-pi) $(objects-pi) $(link-flags)
 $(exe-dtp): $(objects-dtp)
+	$(CC) $(compile-flags) -o $(exe-dtp) $(objects-dtp) $(link-flags)	
 	
 %.o: %.cpp
 	$(CC) $(compile-flags) -c -o $@ $< $(link-flags)
 
-.PHONY: clean pi dtp
+.PHONY: clean pi dtp launcher
 clean:
 	@rm server-dtp/*.o server-pi/*.o || :
 pi: $(objects-pi)
---
+++ launcher.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+
+int main() {   
+    int WritePipe[2], ReadPipe[2];
+    pid_t DTPProcId, PIProcId;
+
+
+    if (pipe(WritePipe) < 0 || pipe(ReadPipe) < 0) {
+        std::cerr << "Could not initiate pipes." << std::endl;
+        return 1;
+    }
+    int writeToPI = ReadPipe[1];
+    int writeToDTP = WritePipe[1];
+    int readFromPI = WritePipe[0];
+    int readFromDTP = ReadPipe[0];
+
+    if ((DTPProcId = fork()) == 0) {
+        close(writeToDTP);
+        close(readFromDTP);
+        char * argv[4];
+        char writeBuf[10], readBuf[10];
+        snprintf(writeBuf, 10, "%d", writeToPI);
+        snprintf(readBuf, 10, "%d", readFromPI);
+        argv[0] = (char*) "server-dtp/dtp";
+        argv[1] = writeBuf;
+        argv[2] = readBuf;
+        argv[3] = (char *) nullptr;
+        execv(argv[0], argv);
+        std::cerr << "Failed to start DTP." << std::endl;
+        return 1;
+
+    }
+    else if (DTPProcId == -1) {
+        std::cerr << "Failed to start DTP." << std::endl;
+        return 1;
+    }
+    close(readFromPI);
+    close(writeToPI);
+
+    if ((PIProcId = fork()) == 0) {
+        char * argv[4];
+        char writeBuf[10], readBuf[10];
+        snprintf(writeBuf, 10, "%d", writeToDTP);
+        snprintf(readBuf, 10, "%d", readFromDTP);
+        argv[0] = (char*) "server-pi/pi";
+        argv[1] = writeBuf;
+        argv[2] = readBuf;
+        argv[3] = (char *) nullptr;
+        execv(argv[0], argv);
+        std::cerr << "Failed to start PI." << std::endl;
+        return 1;
+    }
+    else if (PIProcId == -1) {
+        std::cerr << "Failed to start PI." << std::endl;
+    }
+    close(readFromDTP);
+    close(writeToDTP);
+
+    //wait for either child to return
+    //when one does, kill the other.
+    //(Need to implement a more graceful exit -- i.e. ensure that DTP can complete any transfers)
+    //But this is mainly for bugs    
+    int status;
+    int returnedPID = wait(&status);
+    if (returnedPID == PIProcId) {
+        std::cerr << "PI Crash" << std::endl;
+        kill(DTPProcId, SIGTERM);
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+        if (waitpid(DTPProcId, nullptr, WNOHANG) != DTPProcId) {
+            kill(DTPProcId, SIGKILL);
+            waitpid(DTPProcId, nullptr, 0);
+        }
+    }
+    else {
+        std::cerr << "DTP Crash" << std::endl;
+        kill(PIProcId, SIGTERM);
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+        if (waitpid(PIProcId, nullptr, WNOHANG) != PIProcId) {
+            kill(PIProcId, SIGKILL);
+            waitpid(PIProcId, nullptr, 0);
+        }
+    }
+        
+    return 0;
+}

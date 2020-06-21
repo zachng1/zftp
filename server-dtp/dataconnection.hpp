9c7b689fd97f193c30af884a217193e7e58768d6
Zac Hing <zachng1@gmail.com>
2020-06-21 20:14:53 +1200

    made DataConnection pure abstract

--- server-dtp/dataconnection.hpp
+++ server-dtp/dataconnection.hpp
@@ -1,3 +1,17 @@
+#ifndef __DATACON_H
+#define __DATACON_H
 //represents a single data connection
 //subclass for download vs upload
 
+namespace zftp{
+class DataConnection {
+    public:
+    virtual DataConnection(int fileFd, int sockFd) = 0;
+    virtual int transferFile(int bytes) = 0;
+    
+    private:
+    int sockFd;
+    int fileFd;
+
+};
+}

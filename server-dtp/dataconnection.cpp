e5d57ba15a73fd228d09c2d0b95a33750ea2bbd5
Zac Hing <zachng1@gmail.com>
2020-06-26 19:09:37 +1200

    skeleton for upload and download classes

---
+++ server-dtp/dataconnection.cpp
@@ -0,0 +1,22 @@
+#include "dataconnection.hpp"
+
+namespace zftp{
+DownloadConnection::DownloadConnection(int fd, int fileFD) {
+    
+}
+int DownloadConnection::transferFile(int bytes) {
+
+}
+
+int UploadConnection::transferFile(int bytes) {
+
+}
+
+int newActiveConnection(std::string command) {
+
+}
+
+int newPassiveConnection(std::string command) {
+
+}
+}
--- server-dtp/dataconnection.hpp
+++ server-dtp/dataconnection.hpp
@@ -2,12 +2,41 @@
 #define __DATACON_H
 //represents a single data connection
 //subclass for download vs upload
+#include <string>
+#include <vector>
 
 namespace zftp{
 class DataConnection {
     public:
     virtual int transferFile(int bytes) = 0;
 };
+
+class UploadConnection : public DataConnection {
+    public:
+    UploadConnection(int fd, int fileFD);
+    int transferFile(int bytes);
+
+    private:
+    int fd;
+    int fileSocket;
+};
+
+class DownloadConnection : public DataConnection {
+    public:
+    DownloadConnection(int fd, int fileFD);
+    int transferFile(int bytes);
+
+    private:
+    int fd;
+    int fileSocket;
+};
+
+//Returns a new socket.
+int newActiveConnection(std::vector<std::string> command);
+
+//Returns a new *listening* socket -- make sure it gets closed once
+//the client makes a connection
+int newPassiveConnection(std::vector<std::string> command);
 }
 
 #endif
